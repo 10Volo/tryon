a232d4cc5a1a15e4addc359e275ef2deb3ece50e
knazark <knazark@tenantcloud.com>
2018-07-02 23:03:09 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,41 +2,16 @@
 onePageScroll(".main", {
 	sectionContainer: "section",
 	loop: true,
-	responsiveFallback: false
+	responsiveFallback: false,
+	afterMove: function(index) {
+		var nav = $('nav');
+
+		nav.find('a').removeClass('active');
+		nav.find('a[href="#'+index+'"]').addClass('active');
+	}
 });
 
 // responsive menu toggle
 function menu_toggle(header) {
 	header.classList.toggle('show');
 }
-
-//scroll section, change menu
-$(window).on('scroll', function () {
-	var sections = $('section')
-		, nav = $('nav')
-		, nav_height = nav.outerHeight()
-		, cur_pos = $(this).scrollTop();
-	sections.each(function() {
-		var top = $(this).offset().top - nav_height,
-			bottom = top + $(this).outerHeight();
-		
-		if (cur_pos >= top && cur_pos <= bottom) {
-			nav.find('a').removeClass('active');
-			sections.removeClass('active');
-			
-			$(this).addClass('active');
-			
-			nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-		}
-	});
-});
-
-
-
-
-
-
-
-
-
-
